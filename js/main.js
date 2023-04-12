02df5f32da3b6272a4b777ea93dbe50bae842ddc
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-04-12 17:37:07 +0200

    Aggiunta lettura e scrittura file json

--- js/main.js
+++ js/main.js
@@ -4,12 +4,29 @@ createApp({
     data() {
         return {
             todo: [],
+            newTodo: '',
+        }
+    },
+    methods: {
+        // Metodo che permette di aggiungere un nuovo ToDo
+        Add(){
+            console.log('aggiunto');
+            const data = {
+                newTodo: this.newTodo,
+            }
+            axios.post('php/index.php',data,{
+                headers: {'Content-Type' : 'multipart/form-data'}
+            })
+            .then((response) => {
+                console.log(response);
+                this.todo = response.data;
+            })
         }
     },
     created() {
+        // Chiamata originale all'API
         axios.get('php/index.php')
         .then((response) => {
-            console.log(response);
             this.todo = response.data;
         })
     },
--- php/index.php
+++ php/index.php
@@ -1,7 +1,24 @@
 <?php
-    $todo = file_get_contents(__DIR__.'/../todo.json');
+    // Ricezione di un parametro tramite form in POST
+    $add = isset($_POST['newTodo']) ? $_POST['newTodo'] : NULL;
+    
+    // Lettura e decodifica del file json
+    $todo = file_get_contents(__DIR__.'/todo.json');
     $todo = json_decode($todo,true);
 
+    // Aggiunta del nuovo ToDo
+    if($add !== NULL){
+        $todo[] = [
+            'text' => $add,
+            'done' => false,
+        ];
+    };
+    // Codifica in formato json
+    $todo = json_encode($todo);
+    // Aggiunta del Todo al file json
+    file_put_contents(__DIR__.'/todo.json',$todo);
+    
+    // Risposta in formtato json
     header('Content-Type: application/json');
-    echo json_encode($todo);
+    echo $todo;
 ?>
